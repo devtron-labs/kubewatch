e3c2607ca468c59b5d95d9c1497b96c9dda475cf
Aravind-2023 <aravind@devtron.ai>
2023-05-26 15:06:15 +0530

    kubewatch health api

---
+++ App.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	api "github.com/devtron-labs/kubewatch/api/router"
+	"go.uber.org/zap"
+	"net/http"
+	"os"
+)
+
+type App struct {
+	MuxRouter *api.RouterImpl
+	Logger    *zap.SugaredLogger
+	server    *http.Server
+}
+
+func NewApp(MuxRouter *api.RouterImpl, Logger *zap.SugaredLogger) *App {
+	return &App{
+		MuxRouter: MuxRouter,
+		Logger:    Logger,
+	}
+}
+func (app *App) Start() {
+	port := 8181 //TODO: extract from environment variable
+	app.Logger.Infow("starting server on ", "port", port)
+	app.MuxRouter.Init()
+	server := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: app.MuxRouter.Router}
+	app.server = server
+	err := server.ListenAndServe()
+	if err != nil {
+		app.Logger.Errorw("error in startup", "err", err)
+		os.Exit(2)
+	}
+}
