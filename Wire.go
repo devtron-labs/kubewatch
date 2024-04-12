90c81dedc9359c9fa76ed4fb67de0640948224ad
ShashwatDadhich <dadhichshashwat1808@gmail.com>
2024-04-12 17:49:19 +0530

    fix

--- Wire.go
+++ Wire.go
@@ -5,8 +5,12 @@ package main
 
 import (
 	"github.com/devtron-labs/common-lib/monitoring"
+	pubsub_lib "github.com/devtron-labs/common-lib/pubsub-lib"
 	api "github.com/devtron-labs/kubewatch/api/router"
+	repository "github.com/devtron-labs/kubewatch/pkg/cluster"
+	"github.com/devtron-labs/kubewatch/pkg/informer"
 	"github.com/devtron-labs/kubewatch/pkg/logger"
+	"github.com/devtron-labs/kubewatch/pkg/sql"
 	"github.com/google/wire"
 )
 
@@ -15,7 +19,13 @@ func InitializeApp() (*App, error) {
 		logger.NewSugaredLogger,
 		NewApp,
 		api.NewRouter,
+		sql.NewDbConnection,
+		sql.GetConfig,
+		informer.NewK8sInformerImpl,
+		pubsub_lib.NewPubSubClientServiceImpl,
 		monitoring.NewMonitoringRouter,
+		repository.NewClusterRepositoryImpl,
+		wire.Bind(new(repository.ClusterRepository), new(*repository.ClusterRepositoryImpl)),
 	)
 	return &App{}, nil
 }
